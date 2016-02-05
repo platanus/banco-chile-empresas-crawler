d7d641b400b8a2c615ea2f7b228c8f58d4e6156c
Agustin Feuerhake <agustin@platan.us>
2016-02-05 11:50:13 -0300

    feat():add cuenta corriente reducer

--- app/reducers/cuenta_corriente_reducer.rb
+++ app/reducers/cuenta_corriente_reducer.rb
@@ -1,7 +1,22 @@
+require 'date'
 class CuentaCorrienteReducer < Crabfarm::BaseReducer
 
+  has_array :transactions
+
   def run
-    # Put some data extraction code here.
+    rows = search('#cartolaMovimientos tr')
+    rows = rows.to_a
+    rows.shift
+    rows.pop
+    rows = [rows.first]
+    rows.each do |r|
+      t = Transaction.new()
+      t.amount = r.search("td:nth-child(5)").text.gsub(/[\.]/,"")
+      t.description = r.search("td:nth-child(2)").text
+      t.date = Date.parse(r.search("td:nth-child(1)").text)
+      t.balance = r.search("td:nth-child(7)").text.gsub(/[\.]/,"")
+      self.transactions.push(t)
+    end
   end
 
 end
--- spec/reducers/cuenta_corriente_reducer_spec.rb
+++ spec/reducers/cuenta_corriente_reducer_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
-describe CuentaCorrienteReducer do
+describe CuentaCorrienteReducer, reducing: 'cuenta_corriente', live:true do
 
-  pending "should extract values from snapshot"
+  it "should have a 'transactions' array" do
+    expect(reducer.transactions).to be_instance_of(Array)
+  end
 
+  it "should return transactions" do
+    expect(reducer.transactions.first).to be_instance_of(Transaction)
+  end
+
+  it "should return transactions with information" do
+    expect(reducer.transactions.first.amount).not_to be_nil
+  end
+
+  it "should return a transaction with a valid amount" do
+    expect(reducer.transactions.first.amount).to be_between(-100000000,100000000)
+  end
+
+  it "should return a transaction with a valid description" do
+    expect(reducer.transactions.first.description).to match("[A-Za-z]+")
+  end
+
+  it "should return a transaction with a valid date" do
+    expect(reducer.transactions.first.date).to be_instance_of(Date)
+  end
+
+  it "should return a transaction with a valid balance" do
+    expect(reducer.transactions.first.balance).to be  > 0
+  end
 end
