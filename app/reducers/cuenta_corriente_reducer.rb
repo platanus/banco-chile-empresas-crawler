51695b95f1e27ab6acf40be68e971eaf98aa2dbd
Agustin Feuerhake <agustin@platan.us>
2016-02-13 09:58:18 -0300

    fix(cuenta_corriente): obtain correct amounts

--- app/reducers/cuenta_corriente_reducer.rb
+++ app/reducers/cuenta_corriente_reducer.rb
@@ -8,12 +8,17 @@ class CuentaCorrienteReducer < Crabfarm::BaseReducer
     rows = rows.to_a
     rows.shift
     rows.each do |r|
+      begin
       t = Transaction.new()
-      t.amount = r.search("td:nth-child(5)").text.gsub(/[\.]/,"")
+      t.amount = t.signed_amount(r.search("td:nth-child(5)").text.gsub(/[\.]/,""),r.search("td:nth-child(6)").text.gsub(/[\.]/,""))
       t.description = r.search("td:nth-child(2)").text
       t.date = Date.parse(r.search("td:nth-child(1)").text)
       t.balance = r.search("td:nth-child(7)").text.gsub(/[\.]/,"")
       self.transactions.push(t)
+    rescue
+      binding.pry
+    end
+
     end
   end
 
